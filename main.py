fe88f2fcb8e9ab730bf6947b5169f481e12fecc4
Bilisuma Meressa <bilisumameressa@Billys-Mac.local>
2024-01-02 14:52:16 -0500

    updating stuff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 
+user_choice = ""
+computer_choice = ""
+
 players = [
     {
         "type": "user",
@@ -17,13 +20,18 @@ def winner(player1, player2):
     elif player1 == "R":
         if player2 == "P":
             return player2
-        elif player2 == "S"
+        elif player2 == "S":
             return player1
     elif player1 == "P":
-        
+        if player2 == "R"
+            return player1
+        elif player2 == "S":
+            return player2
     elif player1 == "S":
-    
-        
+        if player2 == "P":
+            return player1
+        elif player2 == "R":
+            return player2
 
 def play():
     choices = ["R", "P", "S"]
